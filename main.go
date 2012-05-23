3042b7721b13cd6f4d1f107f0eb715340906c157
Matt Joiner <anacrolix@gmail.com>
2012-05-23 11:16:17 +0800

    Listens for HTTP and SSDP.

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"crypto/rand"
+	"io"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"net"
+	"log"
+)
+
+func makeDeviceUuid() string {
+	buf := make([]byte, 16)
+	if _, err := io.ReadFull(rand.Reader, buf); err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("uuid:%x-%x-%x-%x-%x", buf[:4], buf[4:6], buf[6:8], buf[8:10], buf[10:])
+}
+
+type server struct {
+	uuid string
+	xmlDesc []byte
+	ssdpConn *net.UDPConn
+	ssdpAddr *net.UDPAddr
+}
+
+type devDescRoot struct {
+	XMLName xml.Name `xml:"root"`
+	Device device `xml:"device"`
+}
+
+type device struct {
+	UDN string
+}
+
+func respondToSSDP(conn *net.UDPConn) {
+	for {
+		b := make([]byte, 4096)
+		n, addr, err := conn.ReadFromUDP(b)
+		log.Println("received ssdp:", n, addr, err, string(b))
+	}
+}
+
+func main() {
+	s := server{
+		uuid: makeDeviceUuid(),
+	}
+	var err error
+	s.xmlDesc, err = xml.MarshalIndent(devDescRoot{Device:device{UDN:s.uuid}}, " ", "  ")
+	if err != nil {
+		panic(err)
+	}
+	log.Println("device description:", string(s.xmlDesc))
+	go func() {
+		if err := http.ListenAndServe(":0", nil); err != nil {
+			panic(err)
+		}
+	}()
+	s.ssdpAddr, err = net.ResolveUDPAddr("udp4", "239.255.255.250:1900")
+	if err != nil {
+		panic(err)
+	}
+	s.ssdpConn, err = net.ListenMulticastUDP("udp4", nil, s.ssdpAddr)
+	if err != nil {
+		panic(err)
+	}
+	respondToSSDP(s.ssdpConn)
+}
