b9a1ad960474d7a2ffded8cd0abda3976d340197
Matt Joiner <anacrolix@gmail.com>
2012-08-26 02:59:22 +1000

    Add a play file to test the SCPD Go types

---
+++ play/scpd.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bitbucket.org/anacrolix/dms/upnp"
+	"encoding/xml"
+	"fmt"
+	"log"
+)
+
+func main() {
+	scpd := upnp.SCPD{
+		SpecVersion: upnp.SpecVersion{Major: 1, Minor: 0},
+		ActionList: []upnp.Action{
+			{
+				Name: "Browse",
+				Arguments: []upnp.Argument{
+					{Name: "ObjectID", Direction: "in", RelatedStateVar: "A_ARG_TYPE_ObjectID"},
+				},
+			},
+		},
+		ServiceStateTable: []upnp.StateVariable{
+			{SendEvents: "no", Name: "A_ARG_TYPE_ObjectID", DataType: "string",
+			AllowedValues: &[]string{"hi", "there"},},
+			{
+				SendEvents: "yes",
+				Name: "loltype",
+			},
+		},
+	}
+	xml, err := xml.MarshalIndent(scpd, "", "  ")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Print(string(xml))
+}
