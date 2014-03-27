b9ed7f12f0304a0783b9d2f9e55ecffa2c4168c8
Matt Joiner <anacrolix@gmail.com>
2014-03-28 00:17:35 +1100

    Tidy up and fix ordering of DLNA content features flags

--- dlna/dlna.go
+++ dlna/dlna.go
@@ -16,33 +16,32 @@ type ContentFeatures struct {
 	ProfileName     string
 	SupportTimeSeek bool
 	SupportRange    bool
-	//play speeds, DLNA.ORG_PS
+	// Play speeds, DLNA.ORG_PS would go here if supported.
 	Transcoded bool
+}
+
+func BinaryInt(b bool) uint {
+	if b {
+		return 1
+	} else {
+		return 0
+	}
 }
 
 // flags are in hex. trailing 24 zeroes, 26 are after the space
 // "DLNA.ORG_OP=" time-seek-range-supp bytes-range-header-supp
-
 func (cf ContentFeatures) String() (ret string) {
 	//DLNA.ORG_PN=[a-zA-Z0-9_]*
-	ret = fmt.Sprintf("DLNA.ORG_OP=%02b;DLNA.ORG_CI=%b", func() (ret uint) {
-		if cf.SupportTimeSeek {
-			ret |= 2
-		}
-		if cf.SupportRange {
-			ret |= 1
-		}
-		return
-	}(), func() uint {
-		if cf.Transcoded {
-			return 1
-		}
-		return 0
-	}())
+	params := make([]string, 0, 2)
 	if cf.ProfileName != "" {
-		ret += ";DLNA_PN=" + cf.ProfileName
+		params = append(params, "DLNA.ORG_PN="+cf.ProfileName)
 	}
-	return
+	params = append(params, fmt.Sprintf(
+		"DLNA.ORG_OP=%b%b;DLNA.ORG_CI=%b",
+		BinaryInt(cf.SupportTimeSeek),
+		BinaryInt(cf.SupportRange),
+		BinaryInt(cf.Transcoded)))
+	return strings.Join(params, ";")
 }
 
 func ParseNPTTime(s string) (time.Duration, error) {
