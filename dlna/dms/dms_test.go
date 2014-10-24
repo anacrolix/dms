6214ee3894b5732ace0ae9c81f18061aeac741cc
Matt Joiner <anacrolix@gmail.com>
2014-10-24 23:39:04 +1100

    Added safeFilePath test cases with volume names for Windows

--- dlna/dms/dms_test.go
+++ dlna/dms/dms_test.go
@@ -1,22 +1,36 @@
 package dms
 
 import (
+	"path/filepath"
+	"runtime"
 	"testing"
 )
 
+type safeFilePathTestCase struct {
+	root, given, expected string
+}
+
 func TestSafeFilePath(t *testing.T) {
-	assertEqual := func(e, a string) {
-		if e != a {
-			t.Fatalf("expected %q but got %q", e, a)
-		}
-	}
-	for _, _case := range []struct {
-		root, given, expected string
-	}{
+	cases := []safeFilePathTestCase{
+		{"/", "..", "/"},
 		{"/hello", "..//", "/hello"},
 		{"", "/precious", "precious"},
 		{".", "///precious", "precious"},
-	} {
-		assertEqual(_case.expected, safeFilePath(_case.root, _case.given))
+	}
+	if runtime.GOOS == "windows" {
+		cases = append(cases, []safeFilePathTestCase{
+			{"c:", "/", "c:/"},
+			{"c:", "/test", "c:/test"},
+			{"c:/", "/", "c:/"},
+			{"c:/", "/test", "c:/test"},
+		}...)
+	}
+	t.Logf("running %d test cases", len(cases))
+	for _, _case := range cases {
+		e := filepath.FromSlash(_case.expected)
+		a := safeFilePath(_case.root, _case.given)
+		if a != e {
+			t.Fatalf("expected %q from %q and %q but got %q", e, _case.root, _case.given, a)
+		}
 	}
 }
