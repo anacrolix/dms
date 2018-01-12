ad17733bc3bccd542c111108dde62c93984b8255
Adirelle <adirelle+github@gmail.com>
2018-01-12 19:22:45 +0100

    Use windows.UTF16PtrFromString instead of a not-working, home-brew func.

--- dlna/dms/dms_windows.go
+++ dlna/dms/dms_windows.go
@@ -4,7 +4,6 @@ package dms
 
 import (
 	"path/filepath"
-	"unicode/utf16"
 
 	"golang.org/x/sys/windows"
 )
@@ -12,7 +11,11 @@ import (
 const hiddenAttributes = windows.FILE_ATTRIBUTE_HIDDEN | windows.FILE_ATTRIBUTE_SYSTEM
 
 func isHiddenPath(path string) (hidden bool, err error) {
-	attrs, err := windows.GetFileAttributes(toWindowsPath(path))
+	winPath, err := windows.UTF16PtrFromString(path)
+	if err != nil {
+		return
+	}
+	attrs, err := windows.GetFileAttributes(winPath)
 	if err == nil {
 		if attrs&hiddenAttributes != 0 {
 			hidden = true
@@ -24,11 +27,6 @@ func isHiddenPath(path string) (hidden bool, err error) {
 		}
 	}
 	return
-}
-
-func toWindowsPath(path string) *uint16 {
-	utf16Path := utf16.Encode([]rune(path))
-	return &(utf16Path[0])
 }
 
 func isReadablePath(path string) (bool, error) {
