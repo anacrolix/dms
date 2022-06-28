092b7b43df261dfe570cfc556941c0e4322e4d29
Chiew <35898533+YanceyChiew@users.noreply.github.com>
2022-06-28 19:22:49 +0800

    [feat](ssdp): Add ip filter callback holder (#96)
    * [feat](ssdp): Add ip filter callback holder
    
    Currently ssdp will announce all available ip addresses on the interface, which may not be what the dlna service actually wants.
    
    The dlna service might not be listening on those addresses at all, which would mislead clients trying to establish a connection.
    
    This commit allows to use a callback function to filter the ip addresses discovered by ssdp to decide whether to use.
    
    * Update ssdp.go
    
    - Modifiy new variable name to be more idiomatic.
    - Fix code formatting issue.

--- ssdp/ssdp.go
+++ ssdp/ssdp.go
@@ -84,6 +84,7 @@ type Server struct {
 	Server         string
 	Services       []string
 	Devices        []string
+	IPFilter       func(net.IP) bool
 	Location       func(net.IP) string
 	UUID           string
 	NotifyInterval time.Duration
@@ -132,6 +133,9 @@ func (me *Server) serve() {
 func (me *Server) Init() (err error) {
 	me.closed = make(chan struct{})
 	me.conn, err = makeConn(me.Interface)
+	if me.IPFilter == nil {
+		me.IPFilter = func(net.IP) bool { return true }
+	}
 	return
 }
 
@@ -158,6 +162,9 @@ func (me *Server) Serve() (err error) {
 				}
 				panic(fmt.Sprint("unexpected addr type:", addr))
 			}()
+			if !me.IPFilter(ip) {
+				continue
+			}
 			if ip.IsLinkLocalUnicast() {
 				// These addresses seem to confuse VLC. Possibly there's supposed to be a zone
 				// included in the address, but I don't see one.
